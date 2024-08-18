b74c7ea36a62e774752321141b455082c8a20a5b
a-sohrabi <sohrabi1372@gmail.com>
2024-08-18 17:25:07 +0330

    feat: load the settings and .env datas in config.py

---
+++ utility/config.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = os.getenv('DATABASE_URL')
+    DATABASE_NAME: str = os.getenv('DATABASE_NAME')
+    NVD_MODIFIED_URL: str = os.getenv('CPE_V23_URL')
+    KAFKA_BOOTSTRAP_SERVERS: str = os.getenv('KAFKA_BOOTSTRAP_SERVER')
+    KAFKA_TOPIC: str = os.getenv('KAFKA_TOPIC')
+    FILES_BASE_DIR: str = 'data'
+
+    class Config:
+        env_file = "../.env"
+
+
+settings = Settings()
