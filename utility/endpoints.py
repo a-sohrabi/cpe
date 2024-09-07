7b09f12420eeea851985e3a7b4b26843c9046a7e
ali_s <ali_s@hfz.local.com>
2024-09-07 17:54:33 +0330

    refact: do some changes on files to get new cpes

--- utility/endpoints.py
+++ utility/endpoints.py
@@ -15,7 +15,7 @@ from .downloader import download_file
 from .extractor import extract_zip
 from .health_check import check_mongo, check_kafka, check_url, check_internet_connection, check_loki
 from .logger import LogManager
-from .parser import parse_json_in_batches
+from .parser import parse_json_in_batches, parse_cpes_from_cve_json_in_batches
 
 load_dotenv()
 
@@ -28,7 +28,7 @@ README_FILE_PATH = Path(__file__).parent.parent / 'README.md'
 
 
 async def download_and_extract(url: str, zip_path: Path, extract_to: Path) -> Path:
-    try:
+    # try:
         await reset_stats()
         logger.info("Starting download and extraction process.")
         await download_file(url, zip_path)
@@ -36,9 +36,9 @@ async def download_and_extract(url: str, zip_path: Path, extract_to: Path) -> Pa
         await extract_zip(zip_path, extract_to)
         logger.info("Extraction completed.")
         return extract_to / zip_path.stem
-    except Exception as e:
-        logger.error(f"Error during download and extraction: {str(e)}")
-        raise  # Re-raise the exception to handle it at a higher level
+    # except Exception as e:
+    #     logger.error(f"Error during download and extraction: {str(e)}")
+    #     raise  # Re-raise the exception to handle it at a higher level
 
 
 async def process_cpes_in_batches(json_file_path: Path):
@@ -83,7 +83,50 @@ async def update_cpes():
         raise  # Re-raise the exception to handle it at a higher level
 
 
-@router.post("/all/")
+async def process_recent_cpes_in_batches(json_file_path: Path):
+    try:
+        logger.info("Starting JSON processing.")
+
+        # Define a semaphore to control the concurrency limit
+        semaphore = asyncio.Semaphore(10)  # Adjust the concurrency as needed
+
+        async def process_batch(batch):
+            async with semaphore:
+                await bulk_create_or_update_cpes(batch)
+
+        # Process batches of CPEs
+        async for batch in parse_cpes_from_cve_json_in_batches(json_file_path):
+            print('*********************',len(batch))
+            logger.info(f"Processing batch of size: {len(batch)}")
+            await process_batch(batch)
+
+        logger.info("Finished processing all CPE batches.")
+    except Exception as e:
+        logger.error(f"Error during processing CPEs in batches: {str(e)}")
+        raise  # Re-raise the exception to handle it at a higher level
+
+
+@record_stats()
+async def update_recent_cpes(feed_type: str):
+    try:
+        base_dir = Path(settings.FILES_BASE_DIR) / 'downloaded'
+        extract_to = base_dir / 'extracted_files'
+        logger.info("Starting CPE update.")
+
+        url = getattr(settings, f"CPE_{feed_type.upper()}_URL")
+        zip_path = base_dir / f'nvdcve-1.1-{feed_type}.json.zip'
+        json_file_path = await download_and_extract(url, zip_path, extract_to)
+
+        logger.info(f"JSON file path: {json_file_path}")
+
+        await process_recent_cpes_in_batches(json_file_path)
+        logger.info("CPE update completed.")
+    except Exception as e:
+        logger.error(f"Error during CPE update: {str(e)}")
+        raise  # Re-raise the exception to handle it at a higher level
+
+
+@router.post("/all")
 async def update_cpes_endpoint(background_tasks: BackgroundTasks, token: str, username: str = Depends(authenticate)):
     if not token == os.getenv('VERIFICATION_TOKEN'):
         return {"error": "Invalid request"}
@@ -91,6 +134,22 @@ async def update_cpes_endpoint(background_tasks: BackgroundTasks, token: str, us
     try:
         logger.info("Received update CPE request.")
         background_tasks.add_task(update_cpes)  # Run this in the background
+        return {"message": 'Started updating CPEs in the background!'}
+    except Exception as e:
+        logger.error(f"Error in update_cpes_endpoint: {str(e)}")
+        return {"error": "Failed to start CPE update."}
+
+
+@router.post("/recent")
+async def update_recent_cpes_endpoint(background_tasks: BackgroundTasks, token: str,
+                                      username: str = Depends(authenticate)):
+    if not token == os.getenv('VERIFICATION_TOKEN'):
+        return {"error": "Invalid request"}
+
+    try:
+        logger.info("Received update CPE request.")
+        background_tasks.add_task(update_recent_cpes, "recent")  # Run this in the background
+        background_tasks.add_task(update_recent_cpes, "modified")
         return {"message": 'Started updating CPEs in the background!'}
     except Exception as e:
         logger.error(f"Error in update_cpes_endpoint: {str(e)}")
@@ -144,9 +203,9 @@ async def get_readme(username: str = Depends(authenticate)):
         logger.error(e)
 
 
-@router.get('/detail/{cpe_id}')
-async def get_detail(cpe_id: str, username: str = Depends(authenticate)):
-    cpe = await get_cpe(cpe_id)
+@router.get('/detail/{cpe_name}')
+async def get_detail(cpe_name: str, username: str = Depends(authenticate)):
+    cpe = await get_cpe(cpe_name)
     if not cpe:
-        return JSONResponse(status_code=404, content={"message": f'{cpe_id} not found'})
+        return JSONResponse(status_code=404, content={"message": f'{cpe_name} not found'})
     return cpe
--- utility/parser.py
+++ utility/parser.py
@@ -49,8 +49,8 @@ async def parse_json_in_batches(json_path: Path, batch_size: int = 2_000) -> Asy
                     target_sw=cpe_obj.get_target_software()[0],
                     target_hw=cpe_obj.get_target_hardware()[0],
                     other=cpe_obj.get_other()[0],
-                    cpe_id=cpe_obj.as_uri_2_3(),  # Use the full URI as the unique ID
-                    type=cpe_type
+                    cpe_name=cpe_obj.as_fs(),
+                    type=cpe_type,
                 )
 
                 batch.append(cpe)
@@ -65,3 +65,66 @@ async def parse_json_in_batches(json_path: Path, batch_size: int = 2_000) -> Asy
         logger.info("JSON streaming parsing completed.")
     except Exception as e:
         logger.error(f"Error while streaming JSON: {str(e)}")
+
+
+async def parse_cpes_from_cve_json_in_batches(json_path: Path, batch_size: int = 50) -> AsyncGenerator[List[CPECreate], None]:
+    try:
+        batch = []
+        async with aiofiles.open(json_path, "rb") as json_file:
+            async for cve_item in ijson.items(json_file, "cve_item"):
+                configurations = cve_item.get("configurations", {})
+                nodes = configurations.get("nodes", [])
+
+                for node in nodes:
+                    cpe_matches = node.get("cpe_match", [])
+
+                    for match in cpe_matches:
+                        cpe23Uri = match.get('cpe23Uri')
+                        if not cpe23Uri:
+                            continue
+
+                        try:
+                            cpe_obj = CPE(cpe23Uri)
+                        except Exception as e:
+                            logger.error(f"Error parsing CPE URI {cpe23Uri}: {str(e)}")
+                            continue
+
+                        if cpe_obj.is_application():
+                            cpe_type = "software"
+                        elif cpe_obj.is_operating_system():
+                            cpe_type = "operating_system"
+                        elif cpe_obj.is_hardware():
+                            cpe_type = "hardware"
+                        else:
+                            cpe_type = 'other'
+
+                        cpe = CPECreate(
+                            cpe_version=cpe_obj.get_version()[0],
+                            part=cpe_obj.get_part()[0],
+                            vendor=cpe_obj.get_vendor()[0],
+                            product=cpe_obj.get_product()[0],
+                            version=cpe_obj.get_version()[0],
+                            update=cpe_obj.get_update()[0],
+                            edition=cpe_obj.get_edition()[0],
+                            language=cpe_obj.get_language()[0],
+                            sw_edition=cpe_obj.get_software_edition()[0],
+                            target_sw=cpe_obj.get_target_software()[0],
+                            target_hw=cpe_obj.get_target_hardware()[0],
+                            other=cpe_obj.get_other()[0],
+                            cpe_name=cpe_obj.as_fs(),
+                            type=cpe_type
+                        )
+                        print(cpe.type)
+
+                        batch.append(cpe)
+
+                        if len(batch) >= batch_size:
+                            yield batch
+                            batch = []
+
+        if batch:
+            yield batch
+
+        logger.info("CPE parsing from CVE JSON completed.")
+    except Exception as e:
+        logger.error(f"Error while streaming JSON for CPEs: {str(e)}")
